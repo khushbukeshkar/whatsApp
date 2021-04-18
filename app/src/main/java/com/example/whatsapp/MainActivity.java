0a6d4be3587dab8f5ce8f693e056ba4359ddb4c4
khushbu <khushbukeshkar79@gmail.com>
2021-04-18 22:35:05 +0530

    Hello Everyone

---
+++ app/src/main/java/com/example/whatsapp/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.whatsapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.whatsapp.Adapters.FragmentsAdapter;
+import com.example.whatsapp.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("Hello, World");
+        auth = FirebaseAuth.getInstance();
+
+        binding.viewPager.setAdapter(new FragmentsAdapter(getSupportFragmentManager()));
+        binding.tablayout.setupWithViewPager(binding.viewPager);
+
+    }
+
+    private Object getSupportActionBarFragmentManager() {
+        return null;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.settings:
+                Intent i = new Intent(MainActivity.this,SettingActivity.class);
+                startActivity(i);
+                break;
+
+            case R.id.logout:
+                auth.signOut();
+                Intent intent = new Intent(MainActivity.this, SignIn.class);
+                startActivity(intent);
+                break;
+
+
+            case R.id.groupChat:
+                Intent intentt = new Intent(MainActivity.this, GroupChatActivity.class);
+                startActivity(intentt);
+                break;
+
+        }
+        return true;
+    }
+}
